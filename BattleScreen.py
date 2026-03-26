4f5b1c443a38d0e9fb0fabb2c765b4a32848e15f
LilyHaaseTV2026 <lilyhaase128@gmail.com>
2026-03-26 11:16:54 -0700

    Temp

--- BattleScreen.py
+++ BattleScreen.py
@@ -4,27 +4,43 @@ from PyUI.PageElements import *
 
 class BattleScreen(Screen):
     def __init__(self, window):
-        super().__init__(window, (25, 255, 40))
+        super().__init__(window, (48, 0, 181))
 
     def addTrainers(self, trainer1Poke, trainer2Poke):
         self.trainers = [
             Trainer(trainer1Poke),
             Trainer(trainer2Poke)
         ]
-        
-    def elementsToDisplay(self):
-        self.elements = []
 
-        y = 0
-        #two rows of three
-        for trainer in self.trainers:
-            x = 0
-            y += 100/3
-            for poke in trainer.pokemon:
-                x += 100/4
-                self.elements.append(Image((x, y), 20, 20, poke.img))
-                self.elements.append(Label((x, y + 10), 20, 10, poke.name))
-                
+    def elementsToDisplay(self):
+        self.elements = [] #new Image with a background (center 50 50 height/width 100/100)
+
+        poke = self.trainers[0].pokemon[0]
+        x = 25
+        y = 15
+        self.elements.append(Image((x, y), 20, 20, poke.img))
+        poke = self.trainers[0].pokemon[1]
+        x = 50
+        y = 15
+        self.elements.append(Image((x, y), 20, 20, poke.img))
+        poke = self.trainers[0].pokemon[2]
+        x = 75
+        y = 15
+        self.elements.append(Image((x, y), 20, 20, poke.img))
+        poke = self.trainers[1].pokemon[0]
+        x = 25
+        y = 85
+        self.elements.append(Image((x, y), 20, 20, poke.img))
+        poke = self.trainers[1].pokemon[1]
+        x = 50
+        y = 85
+        self.elements.append(Image((x, y), 20, 20, poke.img))
+        poke = self.trainers[1].pokemon[2]
+        x = 75
+        y = 85
+        self.elements.append(Image((x, y), 20, 20, poke.img))
+
+
 
 
 
